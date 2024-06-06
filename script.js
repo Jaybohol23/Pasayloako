10b1c64f38c9687ee92505feb21495817f72e7ac
Churchill <141407524+churchillitos@users.noreply.github.com>
2024-06-06 08:18:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', async () => {
     try {
-        const response = await fetch('https://ipinfo.io/136.158.63.54/json?token=60117f9430a2b5');
+        const ip = '136.158.63.54';
+        const response = await fetch(`https://jonellccapisprojectv2-a62001f39859.herokuapp.com/api/ip?ipnum=${ip}`);
         const data = await response.json();
 
         document.getElementById('ip-address').innerText = data.ip;
@@ -13,10 +14,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             document.getElementById('datetime').innerText = currentDate.toLocaleString('en-US', { timeZone: data.timezone });
         }
 
-    
         updateTime();
-
-        
         setInterval(updateTime, 1000);
     } catch (error) {
         console.error('Error fetching IP information:', error);
